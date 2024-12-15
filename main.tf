e9323a4c07a2520552b4a62957949ff926505540
Salvador Menjivar <contact@salvadormenjivar.com>
2024-12-15 12:31:53 -0600

    new update 2.0

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ provider "google" {
   region  = var.region
 }
 
-# Create a testing bucket
 resource "google_storage_bucket" "testing_bucket" {
   name     = var.bucket_name
   location = var.region
