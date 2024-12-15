5d30c231bec688443efcba0c948ea0b9e510e71b
Salvador Menjivar <contact@salvadormenjivar.com>
2024-12-15 10:31:33 -0600

    Updating the env

--- main.tf
+++ main.tf
@@ -1,46 +1,45 @@
+
+
 terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
+      version = "~> 5.0" # Or your preferred version
     }
-  }
-
-  backend "local" {
-    path = "terraform.tfstate"
   }
 }
 
 provider "google" {
-  credentials = var.google_credentials
+  credentials = env("GOOGLE_CREDENTIALS") # Use the environment variable directly
   project     = var.gcp_project_id
   region      = var.region
 }
 
+
 resource "google_compute_instance" "default" {
   name         = "my-linux-vm"
-  machine_type = "e2-medium"                                   # Machine type
-  zone         = "us-central1-a"                              # Specify the zone
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"  # Replace with your desired zone
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2204-jammy-v20230829" # Ubuntu image
+      image = "ubuntu-os-cloud/ubuntu-2204-jammy-v20230829" # Or your preferred image
     }
   }
 
   network_interface {
-    network = "default"                                       # Use the default network
-    access_config {                                          # Create a public IP address
-      // Ephemeral public IP
-    }
+    network = "default"
+    access_config {}  # For an ephemeral IP
   }
 
   metadata_startup_script = <<-EOT
-              #!/bin/bash
-              echo "Hello, World!" > /var/log/startup-script.log
-              EOT
+    #!/bin/bash
+    echo "Hello, World!" > /var/log/startup-script.log
+  EOT
 }
+
 
 output "ip_address" {
   value = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
 }
+
---
+++ variables.tf
@@ -0,0 +1,11 @@
+
+
+variable "gcp_project_id" {
+  description = "The ID of the GCP project"
+  type        = string
+}
+
+variable "region" {
+  description = "The region for GCP resources"
+  type        = string
+}
